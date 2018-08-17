d384123f12ef57918514a5d140fcfaba7cde6aa1
Robert Vojta <robert@resin.io>
2018-08-17 15:44:10 +0200

    Remove unused stuff
    Signed-off-by: Robert Vojta <robert@resin.io>

--- src/lib.rs
+++ src/lib.rs
@@ -5,12 +5,6 @@ extern crate wasm_bindgen;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern "C" {
-    #[wasm_bindgen(js_namespace = console)]
-    fn log(s: &str);
-}
-
-#[wasm_bindgen]
 pub fn plus(a: i32, b: i32) -> i32 {
     a + b
 }
